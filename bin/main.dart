a3241e0d08ab1acff5834d8a1c9c5a5603d7c953
ibonny <ibonny@gmail.com>
2020-06-01 20:30:04 +0000

    Committing my changes to iterate through all variables.

--- bin/main.dart
+++ bin/main.dart
@@ -16,12 +16,25 @@ String testout(int val) {
   return (val * 10).toString();
 }
 
+@Foo(20)
+class MyNewClass {}
+
 void main(List<String> args) {
   print(testout(10));
 
   var mirrors = currentMirrorSystem();
 
+  print(mirrors.isolate.rootLibrary.declarations);
+
   var f = mirrors.isolate.rootLibrary.declarations[Symbol("newValue")];
 
   print(f.metadata.first.reflectee);
+
+  mirrors.isolate.rootLibrary.declarations.forEach((k, v) {
+    if (v.metadata.length > 0) {
+      print("$k, $v, ${v.metadata.first.reflectee}");
+    } else {
+      print("$k, $v");
+    }
+  });
 }
